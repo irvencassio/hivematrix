fc1e5a80ad43d92eb6881a4b05551bddaef3f995
irvcassio <cassio.irv@gmail.com>
2026-06-19 10:27:19 -0400

    Release HiveMatrix 0.1.45 attachment provider parity

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -2,7 +2,7 @@
  * HiveMatrix version metadata, surfaced in Settings.
  * Bump BUILD_NUMBER + BUILD_DATE when cutting a build.
  */
-export const VERSION = "0.1.44";
+export const VERSION = "0.1.45";
 export const BUILD_NUMBER = 635;
 export const BUILD_DATE = "2026-06-18";
 
